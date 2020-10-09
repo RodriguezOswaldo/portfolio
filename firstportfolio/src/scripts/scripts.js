aa39e7836398634b7451dd9acbb8d9c9ae363ba5
Oswaldo Rodriguez <owner2303@gmail.com>
2020-10-09 01:26:15 -0400

    beautiful switch statement

--- firstportfolio/src/scripts/scripts.js
+++ firstportfolio/src/scripts/scripts.js
@@ -6,6 +6,41 @@ console.log('it is working');
 let themeDots = document.getElementsByClassName('theme-dot');
 for (var i=0; themeDots.length > i; i++){
     themeDots[i].addEventListener('click', function(){
-        console.log('I was clicked!');
+        let mode = this.dataset.mode;
+        console.log('I was clicked!', mode);
+        setTheme(mode);
     });
 }
+function setTheme(mode){
+     switch (mode) {
+        case 'light':
+        document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
+        break;
+        case 'purple':
+            document.getElementById('theme-style').href = '/firstportfolio/src/css/purple.css';
+        break;
+        case 'blue':
+            document.getElementById('theme-style').href = '/firstportfolio/src/css/blue-dot.css';
+        break;
+        case 'green':
+            document.getElementById('theme-style').href = '/firstportfolio/src/css/green.css';
+        break;
+
+        default:
+            document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
+            break;
+    }
+    // if(mode == light){
+    //     document.getElementsById('theme-style').href = '/firstportfolio/src/css/default.css';
+    // }
+    // if(mode == purple){
+    //     document.getElementsById('theme-style').href = '/firstportfolio/src/css/purple.css';
+    // }
+    // if(mode == blue){
+    //     document.getElementsById('theme-style').href = '/firstportfolio/src/css/blue-dot.css';
+    // }
+    // if(mode == green){
+    //     document.getElementsById('theme-style').href = '/firstportfolio/src/css/green.css';
+    // }
+}
+   
