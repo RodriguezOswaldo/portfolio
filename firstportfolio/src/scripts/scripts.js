e569a1ccbe0ce6a560730e73a7ff8b4e52431c82
Oswaldo Rodriguez <owner2303@gmail.com>
2020-10-09 01:09:58 -0400

    adding data to the themes

--- firstportfolio/src/scripts/scripts.js
+++ firstportfolio/src/scripts/scripts.js
@@ -1,3 +1,11 @@
 // Tutorial comming from Traversy Media 
 // https://www.youtube.com/watch?v=r_hYR53r61M&ab_channel=TraversyMedia
 
+console.log('it is working');
+
+let themeDots = document.getElementsByClassName('theme-dot');
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        console.log('I was clicked!');
+    });
+}
