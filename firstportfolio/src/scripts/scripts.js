cfa95b8132127d90677f4a2b362dfbdf5715ec30
Oswaldo Rodriguez <owner2303@gmail.com>
2020-10-09 01:29:43 -0400

    local storage :) setting theme

--- firstportfolio/src/scripts/scripts.js
+++ firstportfolio/src/scripts/scripts.js
@@ -1,7 +1,12 @@
 // Tutorial comming from Traversy Media 
 // https://www.youtube.com/watch?v=r_hYR53r61M&ab_channel=TraversyMedia
 
-console.log('it is working');
+let theme = localStorage.getItem('theme');
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme )
+}
 
 let themeDots = document.getElementsByClassName('theme-dot');
 for (var i=0; themeDots.length > i; i++){
@@ -30,5 +35,6 @@ function setTheme(mode){
             document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
             break;
     }
+    localStorage.setItem('theme', mode);
 }
    
