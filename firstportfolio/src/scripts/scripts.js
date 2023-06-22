b9d091b7fe8b46546ceeb1d12191cfe690313e37
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-06-22 11:14:24 -0400

    added submit page

--- firstportfolio/src/scripts/scripts.js
+++ firstportfolio/src/scripts/scripts.js
@@ -2,34 +2,34 @@
 // https://www.youtube.com/watch?v=r_hYR53r61M&ab_channel=TraversyMedia
 
 let theme = localStorage.getItem('theme');
-if(theme == null){
+if (theme == null) {
     setTheme('light')
-}else{
-    setTheme(theme )
+} else {
+    setTheme(theme)
 }
 
 let themeDots = document.getElementsByClassName('theme-dot');
-for (var i=0; themeDots.length > i; i++){
-    themeDots[i].addEventListener('click', function(){
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
         let mode = this.dataset.mode;
         console.log('I was clicked!', mode);
         setTheme(mode);
     });
 }
-function setTheme(mode){
-     switch (mode) {
+function setTheme(mode) {
+    switch (mode) {
         case 'light':
-        document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
-        break;
+            document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
+            break;
         case 'purple':
             document.getElementById('theme-style').href = '/firstportfolio/src/css/purple.css';
-        break;
+            break;
         case 'blue':
             document.getElementById('theme-style').href = '/firstportfolio/src/css/blue-dot.css';
-        break;
+            break;
         case 'green':
             document.getElementById('theme-style').href = '/firstportfolio/src/css/green.css';
-        break;
+            break;
 
         default:
             document.getElementById('theme-style').href = '/firstportfolio/src/css/default.css';
@@ -37,4 +37,16 @@ function setTheme(mode){
     }
     localStorage.setItem('theme', mode);
 }
-   
+
+function auth() {
+
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var email = document.getElementById("email").value;
+
+    //     window.location.assign("submit.html");
+    //     if (name.length != 0 || email.length != 0) {
+    //     } else {
+    //         alert("Fields must be completed before submitting the form")
+    //     }
+}
